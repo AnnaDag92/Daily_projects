5f5cf8f6f668eb47f772900c1703667483433034
AnnaDag92 <78444190+AnnaDag92@users.noreply.github.com>
2021-06-19 18:54:27 +0400

    Add files via upload

---
+++ MergeSort.cpp
@@ -0,0 +1,58 @@
+﻿#include <iostream>
+#include <vector>
+
+void Merge(std::vector<int>& vec, int p, int q, int r) {
+	int size1 = q - p + 1;
+	int size2 = r - q;
+	std::vector<int> L(size1);
+	std::vector<int> R(size2);
+	int x = p;
+	for (int i = 0; i < L.size(); ++i) {
+		L[i] = vec[x++];
+	}
+	for (int i = 0; i < R.size(); ++i) {
+		R[i] = vec[x++];
+	}
+	int i = 0; 
+	int j = 0;
+	int k = p;
+	for ( ; i < L.size() && j < R.size(); ++k) {
+		if ( L[i] <= R[j]) {
+			vec[k] = L[i++];
+		}
+		else {
+			vec[k] = R[j++];
+		}
+	}
+	while (i < L.size()) {
+		vec[k] = L[i];
+		++i;
+		++k;
+	}
+	while (j < R.size()) {
+		vec[k] = R[j];
+		++j;
+		++k;
+	}
+}
+
+void MergeSort(std::vector<int>& vec, int p, int r) {
+	if (p < r) {
+		int q = (p + r) / 2;
+		MergeSort(vec, p, q);
+		MergeSort(vec, q + 1, r);
+		Merge(vec, p, q, r);
+	}
+}
+void Print(std::vector<int>& vec) {
+	for (auto i : vec) {
+		std::cout << i;
+	}
+}
+
+int main()
+{
+	std::vector<int> vec {2,5,9,4,3,6,7,8};
+	MergeSort(vec, 0, vec.size() - 1);
+	Print(vec);
+}
---
+++ QuickSort.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <vector>
+
+int  Partition(std::vector<int>& vec, int p, int r) {
+	int pivot = r;
+	int i = p;
+	int j = r - 1;
+	while (i <= j) {
+		if (vec[j] > vec[pivot]) {
+			std::swap(vec[j], vec[i]);
+			++i;
+		}
+		else {
+			--j;
+		}
+	}
+	std::swap(vec[i], vec[pivot]);
+	return i;
+}
+void QuickSort(std::vector<int>& vec, int p, int r) {
+	if (p < r) {
+		int q = Partition(vec, p, r);
+		QuickSort(vec, p, q - 1);
+		QuickSort(vec, q + 1, r);
+	}
+}
+void Print(std::vector<int>& vec) {
+	for (auto i : vec) {
+		std::cout << i;
+	}
+}
+
+int main()
+{
+	std::vector<int> vec {2,5,9,4,3,8,0,-4,6,8};
+	QuickSort(vec, 0, vec.size() - 1);
+	Print(vec);
+}
